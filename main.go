a03ac06b887f9f6e4aff58719a618fe288d2af37
Zack Scholl <zack.scholl@gmail.com>
2019-01-15 05:32:16 +0000

    change port

--- main.go
+++ main.go
@@ -61,5 +61,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8222", nil))
 }
