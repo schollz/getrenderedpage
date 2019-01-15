43d77347e29dc81fac09f927c0ab9ec657aad620
Zack Scholl <zack.scholl@gmail.com>
2019-01-15 08:21:19 -0800

    how to return from async object

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 // content of index.js
 const http = require('http')
+const puppeteer = require('puppeteer');
 const port = 3000
 
 const requestHandler = (request, response) => {
-  if (request.url[1:].startsWith(http)) {
-    response.writeHead(200,{'Content-Type': 'text/plain'});
-    response.end(request.url);
+    let request_url = request.url.substring(1, request.url.length);
+    console.log(request_url);
+    if (request_url.startsWith("http")) {
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.end(request_url);
 
-  } else {
-    response.writeHead(500,{'Content-Type': 'text/plain'});
-    response.end("error");
-
-  }
-  console.log(request.url)
+    } else {
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.end("error");
+    }
 }
+
+// (async() => {
+// 	const browser = await puppeteer.launch({headless:true});
+// 	const page = await browser.newPage();
+// 	await page.goto(process.argv[2]);
+//     await page.waitFor(500);
+// 	let content = await page.content();
+// 	console.log(content);
+// 	browser.close();
+// })();
 
 const server = http.createServer(requestHandler)
 
 server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
+    if (err) {
+        return console.log('something bad happened', err)
+    }
 
-  console.log(`server is listening on ${port}`)
+    console.log(`server is listening on ${port}`)
 })
